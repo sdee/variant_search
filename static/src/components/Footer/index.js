2b5d3d0ff60f30894b20db6fd1e51fc2e4a322db
Sutee Dee <thaiyoshi@gmail.com>
2017-06-30 19:46:01 -0700

    removes uneeded code in client

--- static/src/components/Footer/index.js
+++ static/src/components/Footer/index.js
@@ -8,7 +8,7 @@ export const Footer = () =>
         <div className="container">
             <div className="row">
                 <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <p>© DZT 2016</p>
+                    
                 </div>
             </div>
         </div>
