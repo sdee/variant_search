936ae339841cfe04375f10095f16d38f03444a1e
Sutee Dee <thaiyoshi@gmail.com>
2017-06-30 19:37:03 -0700

    removes uneeded reducers, routes, actions, and components

--- static/src/components/Header/index.js
+++ static/src/components/Header/index.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import AppBar from 'material-ui/AppBar';
 import MenuItem from 'material-ui/MenuItem';
 import FlatButton from 'material-ui/FlatButton';
 import Divider from 'material-ui/Divider';
-
-import * as actionCreators from '../../actions/auth';
 
 function mapStateToProps(state) {
     return {
@@ -16,53 +13,25 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators(actionCreators, dispatch);
+
 }
 
 @connect(mapStateToProps, mapDispatchToProps)
 export class Header extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            open: false,
-        };
-
     }
 
     dispatchNewRoute(route) {
         browserHistory.push(route);
-        this.setState({
-            open: false,
-        });
-
-    }
-
-    handleClickOutside() {
-        this.setState({
-            open: false,
-        });
-    }
-
-
-    logout(e) {
-        e.preventDefault();
-        this.props.logoutAndRedirect();
-        this.setState({
-            open: false,
-        });
-    }
-
-    openNav() {
-        this.setState({
-            open: true,
-        });
     }
 
     render() {
         return (
             <header>
                 <AppBar
-                  title="React-Redux-Flask"
+                  title="Variant Search"
+									showMenuIconButton={false}
                 />
             </header>
 
--- static/src/routes.js
+++ static/src/routes.js
@@ -1,18 +1,19 @@
 /* eslint new-cap: 0 */
 
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
 /* containers */
 import { App } from './containers/App';
 import { HomeContainer } from './containers/HomeContainer';
-i
 import NotFound from './components/NotFound';
 
 
 export default (
     <Route path="/" component={App}>
-        <Route path="home" component={requireNoAuthentication(HomeContainer)} />
+				<IndexRoute component={HomeContainer}/>
+        <Route path="home" component={HomeContainer} />
+				<Route path="search" component={HomeContainer} />
         <Route path="*" component={NotFound} />
     </Route>
 );
