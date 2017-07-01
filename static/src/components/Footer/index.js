62e2115a9980a950a203f1001609326d4feff980
Sutee Dee <thaiyoshi@gmail.com>
2017-07-01 01:58:17 -0700

    removes uneeded boilerplate server-side code

--- static/src/components/Footer/index.js
+++ static/src/components/Footer/index.js
@@ -8,7 +8,6 @@ export const Footer = () =>
         <div className="container">
             <div className="row">
                 <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    
                 </div>
             </div>
         </div>
