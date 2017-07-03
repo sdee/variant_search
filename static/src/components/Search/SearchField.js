cabce04ef63a489da92f2eb796009d39a728e57b
Sutee Dee <thaiyoshi@gmail.com>
2017-07-02 23:21:45 -0700

    displays 'no matches' when there are no suggestions

--- static/src/components/Search/SearchField.js
+++ static/src/components/Search/SearchField.js
@@ -7,96 +7,95 @@ import ActionGrade from 'material-ui/svg-icons/action/grade';
 import RaisedButton from 'material-ui/RaisedButton';
 import axios from 'axios';
 
-
-const getSuggestionValue = suggestion =>  suggestion;
+const getSuggestionValue = suggestion => suggestion;
 
 const renderInputComponent = inputProps => (
-  <TextField
-    hintText="Type a gene name"
-    style={{
-      width: '100%',
-      fontSize: '16px',
-    }}
-    {...inputProps}
+    <TextField
+      hintText="Type a gene name"
+      style={{
+          width: '100%',
+          fontSize: '16px',
+      }}
+      {...inputProps}
     />
 );
 
 class SearchField extends React.Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    // Autosuggest is a controlled component.
-    // This means that you need to provide an input value
-    // and an onChange handler that updates this value (see below).
-    // Suggestions also need to be provided to the Autosuggest,
-    // and they are initially empty because the Autosuggest is closed.
-    this.state = {
-      value: '',
-      suggestions: [],
-      isLoading: false
-    };
+        this.state = {
+          value: '',
+          suggestions: [],
+          isLoading: false,
+      };
 
-    this.lastRequestId = null;
+      this.lastRequestId = null;
 
   }
 
-  onChange = (event, { newValue }) => {
-    this.setState({
-      value: newValue
-    });
-  };
+    onChange = (event, { newValue }) => {
+        this.setState({
+            value: newValue,
+        });
+    };
 
   // Autosuggest will call this function every time you need to update suggestions.
   // You already implemented this logic above, so just use it.
-  onSuggestionsFetchRequested = ({ value }) => {
-    this.loadSuggestions(value);
-  };
-
-loadSuggestions = (value) => {
-  if (this.lastRequestId !== null) {
-    clearTimeout(this.lastRequestId);
-  }
-
-  this.setState({
-    isLoading: true
-  });
-
-  this.lastRequestId = setTimeout(() => {
-      axios.get('/api/suggestions/'+value)
-          .then(({ data })=> {
-          	this.setState({suggestions: data.results, isLoading: false})
-          })
-          .catch((err)=> {})
-
-  }, 1000);
-};
-
-  // Autosuggest will call this function every time you need to clear suggestions.
-  onSuggestionsClearRequested = () => {
-    this.setState({
-      suggestions: []
-    });
-  };
-
-  render() {
-    const { value, suggestions, isLoading } = this.state;
-
-    // Autosuggest will pass through all these props to the input.
-    const inputProps = {
-      value,
-      onChange: this.onChange
+    onSuggestionsFetchRequested = ({ value }) => {
+        this.loadSuggestions(value);
     };
 
-    return (
-        <div>
-          <Autosuggest
-            suggestions={suggestions}
-            onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-            onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-            getSuggestionValue={getSuggestionValue}
-            renderSuggestionsContainer={({ children, ...rest }) => (
-                <List {...rest}>
-                    {children}
+    loadSuggestions = (value) => {
+        if (this.lastRequestId !== null) {
+            clearTimeout(this.lastRequestId);
+        }
+        this.setState({
+            isLoading: true,
+        });
+
+        this.lastRequestId = setTimeout(() => {
+            axios.get(`/api/suggestions/${value}`)
+          .then(({ data }) => {
+              if (data.results.length > 0) {
+                this.setState({ suggestions: data.results, isLoading: false, noSuggestionsAvailable: false });
+              }              else {
+                this.setState({ noSuggestionsAvailable: true, isLoading: false, suggestions: [] });
+              }
+
+          })
+          .catch((err) => { console.log(err); });
+
+        }, 1000);
+    };
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+    onSuggestionsClearRequested = () => {
+        this.setState({
+            suggestions: [],
+            noSuggestionsAvailable: false
+        });
+    };
+
+    render() {
+        const { value, suggestions, isLoading, noSuggestionsAvailable } = this.state;
+       console.log(noSuggestionsAvailable);
+    // Autosuggest will pass through all these props to the input.
+        const inputProps = {
+            value,
+            onChange: this.onChange,
+        };
+
+        return (
+            <div>
+                <Autosuggest
+                  suggestions={suggestions}
+                  onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                  onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                  getSuggestionValue={getSuggestionValue}
+                  renderSuggestionsContainer={({ children, ...rest }) => (
+                      <List {...rest}>
+                          {children}
                 </List>
         )}
             renderSuggestion={suggestion => (
@@ -109,6 +108,16 @@ loadSuggestions = (value) => {
             inputProps={inputProps}
             renderInputComponent={inputProps => renderInputComponent(inputProps)}
           />
+          {
+            noSuggestionsAvailable &&
+              <div className="no-suggestions">
+                <ListItem
+                  primaryText={"No Matches"}
+                  
+                  style={{ textAlign: 'left' }}
+                />
+              </div>
+          }
             <RaisedButton label="Search" primary />
         </div>
     );
