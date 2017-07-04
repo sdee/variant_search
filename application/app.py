e95c70a17b88e568ad6f78c2f1a1188664aa0c3c
Sutee Dee <thaiyoshi@gmail.com>
2017-07-03 21:31:36 -0700

    reorganizes gene name suggstion logic into class

--- application/app.py
+++ application/app.py
@@ -1,10 +1,25 @@
 import json
 import unicodecsv
 from collections import defaultdict
-from flask import request, g, abort
+from flask import g, abort
 import marisa_trie
 from index import app, db
 from sets import Set
+
+
+class GeneNameStore:
+
+    def __init__(self, gene_names):
+        self.trie = marisa_trie.Trie(gene_names)
+
+    def get_suggestions(self, query, strict_casing=False):
+        def has_lowercase(query):
+            return any(filter(str.islower, str(query)))
+        suggestions = self.trie.keys(unicode(query))
+        if not strict_casing and not suggestions and has_lowercase(query):
+            suggestions = self.trie.keys(unicode(str(query).upper()))
+        suggestions.sort()  # sort to ensure consistent results every time
+        return suggestions
 
 
 @app.before_request
@@ -17,27 +32,13 @@ def initialize_search_data():
             gene = line['Gene']
             genes.add(gene)
             variants_by_gene[gene].append(line)
-    g.gene_name_trie = _build_trie(genes)
+    g.gene_name_store = GeneNameStore(genes)
     g.variants_by_gene = variants_by_gene
-
-
-def _build_trie(gene_names):
-    return marisa_trie.Trie(gene_names)
-
-
-def get_gene_name_suggestions(query, strict_casing=False):
-    def has_lowercase(query):
-        return any(filter(str.islower, str(query)))
-    suggestions = g.gene_name_trie.keys(unicode(query))
-    if not strict_casing and not suggestions and has_lowercase(query):
-        suggestions = g.gene_name_trie.keys(unicode(str(query).upper()))
-    suggestions.sort()  # sort to ensure consistent results every time
-    return suggestions
 
 
 @app.route('/api/suggestions/<fragment>', methods=['GET'])
 def suggestions_endpoint(fragment=None):
-    suggestions = get_gene_name_suggestions(fragment) if fragment else []
+    suggestions = g.gene_name_store.get_suggestions(fragment) if fragment else []
     return json.dumps(dict(results=[str(s) for s in suggestions]))
 
 
