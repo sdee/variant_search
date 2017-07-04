58f75c65f2a6a79b3d0beb4492140e415af64346
Sutee Dee <thaiyoshi@gmail.com>
2017-07-04 12:28:57 -0700

    adds react-data-grid

--- static/src/components/Results/ResultsTable.js
+++ static/src/components/Results/ResultsTable.js
@@ -1,12 +1,45 @@
 import React from "react";
+import axios from 'axios';
 
 export default class ResultsTable extends React.Component {
+
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        geneName: '',
+        variants: []
+      };
+    }
+
+  componentDidMount() {
+    console.log("mount");
+    console.log(this.props.params.geneName);
+    console.log(`/api/gene/${this.props.params.geneName}`)
+     axios.get(`/api/variants/${this.props.params.geneName}`)
+       .then(({ data }) => {
+         console.log(data);
+        this.setState({ variants: data });
+       });
+   }
+
   render() {
     const { query } = this.props.location;
     const { params } = this.props;
     const {geneName} = params;
-    console.log(this.props);
-    console.log(geneName);
+
+    let columns = [
+  {
+    key: 'Gene',
+    name: 'id',
+    locked: true
+  },
+  {
+    key: 'Nucleotide Change',
+    name: 'Nucleotide Change'
+  }
+];
+
     return (
       <div>
         <h1>Test</h1>
