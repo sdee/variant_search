35e7d12f7d71e7b889fd2b05f528d2be52dcfb8b
Sutee Dee <thaiyoshi@gmail.com>
2017-07-04 13:20:59 -0700

    adds columns to data table

--- static/src/components/Results/ResultsTable.js
+++ static/src/components/Results/ResultsTable.js
@@ -37,11 +37,49 @@ export default class ResultsTable extends React.Component {
   {
     key: 'Gene',
     name: 'Gene',
-    locked: true
+    locked: true,
+    width: 80
   },
   {
     key: 'Nucleotide Change',
-    name: 'Nucleotide Change'
+    name: 'Nucleotide Change',
+    width: 350,
+    resizable: true
+  },
+  {
+    key: 'Protein Change',
+    name: 'Protein Change',
+    width: 80
+  },
+  {
+    key: 'Alias',
+    name: 'Alias',
+    width: 80
+  },
+  {
+    key: 'Region',
+    name: 'Region',
+    width: 100
+  },
+  {
+    key: 'Reported Classification',
+    name: 'Reported Classification',
+    width: 200
+  },
+  {
+    key: 'Last Evaluated',
+    name: 'Last Evaluated',
+    width: 130
+  },
+  {
+    key: 'Last Updated',
+    name: 'Last Updated',
+    width: 130
+  },
+  {
+    key: 'URL',
+    name: 'More info',
+    width: 200
   }
 ];
 
@@ -53,8 +91,8 @@ export default class ResultsTable extends React.Component {
       columns={columns}
       rowGetter={this.getRows}
       rowsCount={this.state.variants.length}
-
-      minHeight={500}
+      minWidth={1350}
+      minHeight={300}
       />
       </div>
     );
