4d57ccf31e1c743a5d0956b240bcd79ea5146ea9
Sutee Dee <thaiyoshi@gmail.com>
2017-07-05 16:14:02 -0700

    cleanup

--- static/src/routes.js
+++ static/src/routes.js
@@ -1,13 +1,11 @@
-/* eslint new-cap: 0 */
-
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import { App } from './containers/App';
 import { HomeContainer } from './containers/HomeContainer';
-import { Results } from './containers/results/Results';
 import NotFound from './components/NotFound';
 import ResultsTable from './components/Results/ResultsTable';
+
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={HomeContainer} />
