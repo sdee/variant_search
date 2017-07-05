f6c0407dd66516ccf1167d8c9963d316aa690b88
Sutee Dee <thaiyoshi@gmail.com>
2017-07-05 14:31:29 -0700

    formatting for config

--- static/karma.conf.js
+++ static/karma.conf.js
@@ -1,39 +1,38 @@
 module.exports = function (config) {
-  config.set({
-    basePath: 'src',
-    singleRun: true,
-    frameworks: ['mocha'],
-    reporters: ['dots'],
-    browsers: ['Chrome'],
-    files: [
-      'tests/**/*.spec.js',
-    ],
-    preprocessors: {
-      'tests/**/*.spec.js': ['webpack'],
-    },
-    webpack: {
-      resolve: {
-        extensions: ['', '.js', '.ts'],
-        modulesDirectories: ['node_modules', 'src'],
-      },
-      devtool: 'inline-source-map', // just do inline source maps instead of the default
-      module: {
-        loaders: [{
-          test: /\.js$/,
-          exclude: /\/node_modules\//,
-          loader: 'babel',
-          query: {
-            presets: ['airbnb'],
-          },
-        }],
-      },
-      externals: {
-        cheerio: 'window',
-        'react/addons': true,
-        'react/lib/ExecutionEnvironment': true,
-        'react/lib/ReactContext': true,
-      },
-     },
-     // ...
-   });
- };
+    config.set({
+        basePath: 'src',
+        singleRun: true,
+        frameworks: ['mocha'],
+        reporters: ['dots'],
+        browsers: ['Chrome'],
+        files: [
+            'tests/**/*.spec.js',
+        ],
+        preprocessors: {
+            'tests/**/*.spec.js': ['webpack'],
+        },
+        webpack: {
+            resolve: {
+                extensions: ['', '.js', '.ts'],
+                modulesDirectories: ['node_modules', 'src'],
+            },
+            devtool: 'inline-source-map', // just do inline source maps instead of the default
+            module: {
+                loaders: [{
+                    test: /\.js$/,
+                    exclude: /\/node_modules\//,
+                    loader: 'babel',
+                    query: {
+                        presets: ['airbnb'],
+                    },
+                }],
+            },
+            externals: {
+                cheerio: 'window',
+                'react/addons': true,
+                'react/lib/ExecutionEnvironment': true,
+                'react/lib/ReactContext': true,
+            },
+        },
+    });
+};
