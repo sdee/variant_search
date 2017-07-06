69fe4fff5ec0e73e5591f3c0ce2dbbd80aee8c37
Sutee Dee <thaiyoshi@gmail.com>
2017-07-06 14:52:58 -0700

    config for default to variants file with 2000 variants

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from setup import basedir
 class BaseConfig(object):
     SECRET_KEY = "SO_SECURE"
     DEBUG = True
-    VARIANT_FILE_PATH = 'data/variant_results.tsv'
+    VARIANT_FILE_PATH = 'data/variant_results_2000_variants.tsv'
 
 
 class TestingConfig(object):
@@ -17,4 +17,4 @@ class TestingConfig(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     DEBUG_TB_ENABLED = True
     PRESERVE_CONTEXT_ON_EXCEPTION = False
-    VARIANT_FILE_PATH = 'data/variant_results.tsv'
+    VARIANT_FILE_PATH = 'data/variant_results_2000_variants.tsv'
